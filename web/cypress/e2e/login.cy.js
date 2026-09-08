eeab40c6fdab554455b4da4a3f3a9bf7e719921a
Isaque França de Oliveira Beltrão <isaquefrnoliv@gmail.com>
2026-09-08 19:17:22 -0300

    feat: Implementar Primeiro Test

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login',() => {
+  it( 'Deve logar com sucesso', () => {
     cy.viewport(1440,900)
     cy.visit('http://localhost:3000')
   })
-})
+})  
